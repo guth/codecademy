e4bc5167a6df084d396274c7454f7809d437db09
Alex Guthrie <guthriea90@gmail.com>
2018-02-13 15:05:00 -0800

    Add the ability to flip tiles and count the number of neighboring bombs

--- BuildFrontEndWebApplicationsFromScratch/minesweeper/src/minesweeper.js
+++ BuildFrontEndWebApplicationsFromScratch/minesweeper/src/minesweeper.js
@@ -36,13 +36,60 @@ const generateBombBoard = (numRows, numColumns, numBombs) => {
 		let row = Math.floor(Math.random() * numRows);
 		let column = Math.floor(Math.random() * numColumns);
 		
-		// This can place multiple bombs in the same place and not count it.
-		// This will be fixed later.
-		board[row][column] = "B";
-		numBombsPlaced++;
+		if(board[row][column] !== "B")
+		{
+			board[row][column] = "B";
+			numBombsPlaced++;
+		}
 	}
 
 	return board;
+};
+
+const getNumberOfNeighborBombs = (board, row, column) => {
+	const neighborOffsets = [
+		[-1, -1], [0, -1], [1, -1],
+		[-1, 0],         , [1, 0],
+		[-1, 1],  [0, 1],  [1, 1],
+		];
+
+	const numRows = board.length;
+	const numColumns = board[0].length;
+
+	let numberOfBombs = 0;
+
+	neighborOffsets.forEach(offset => {
+		const nextRow = row + offset[0];
+		const nextColumn = column + offset[1];
+
+		if(nextRow >= 0 && nextRow < numRows
+			&& nextColumn >= 0 && nextColumn < numColumns)
+		{
+			if(board[nextRow][nextColumn] === "B")
+			{
+				numberOfBombs++;
+			}
+		}
+	});
+
+	return numberOfBombs;
+};
+
+const flipTile = (playerBoard, bombBoard, row, column) => {
+	if(playerBoard[row][column] !== " ")
+	{
+		console.log("This tile has already been flipped!");
+		return;
+	}
+	else if(bombBoard[row][column] === "B")
+	{
+		playerBoard[row][column] = "B";
+	}
+	else
+	{
+		let numBombs = getNumberOfNeighborBombs(bombBoard, row, column);
+		playerBoard[row][column] = numBombs;
+	}
 };
 
 // let board = [
@@ -59,3 +106,7 @@ printBoard(playerBoard);
 
 console.log("Bomb Board:");
 printBoard(bombBoard);
+
+flipTile(playerBoard, bombBoard, 0, 0);
+console.log("Updated player board:");
+printBoard(playerBoard);
