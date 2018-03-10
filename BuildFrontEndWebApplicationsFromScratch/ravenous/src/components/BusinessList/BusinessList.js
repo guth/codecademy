b55fecf030f4d319e3441b89637ac3c3e7ab9514
Alex Guthrie <guthriea90@gmail.com>
2018-03-09 16:02:03 -0800

    Add an alternative render one-liner to BusinessList based on the solution

--- BuildFrontEndWebApplicationsFromScratch/ravenous/src/components/BusinessList/BusinessList.js
+++ BuildFrontEndWebApplicationsFromScratch/ravenous/src/components/BusinessList/BusinessList.js
@@ -4,6 +4,19 @@ import Business from '../Business/Business';
 
 class BusinessList extends React.Component
 {
+	renderOneLine()
+	{
+	    return (
+	      <div className="BusinessList">
+	        {
+	          this.props.businesses.map(business => {
+	            return <Business business={business} />
+	          })
+	        }
+	      </div>
+	    );
+  	}
+
 	render()
 	{
 		const businessComponents = this.props.businesses.map(function(business) {
