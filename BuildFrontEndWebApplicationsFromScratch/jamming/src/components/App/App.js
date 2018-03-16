a48d58a5e2c203df4ca19e72cd37a6c69c42207e
Alex Guthrie <guthriea90@gmail.com>
2018-03-15 17:30:16 -0700

    Update the jamming template code

---
+++ BuildFrontEndWebApplicationsFromScratch/jamming/src/components/App/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component
+{
+    render()
+    {
+        return (
+            <div className="App">
+                <h1>Ja<span class="highlight">mmm</span>ing</h1>
+            </div>
+        );
+    }
+    // render() {
+    //     return (
+    //         <div className="App">
+    //             <header className="App-header">
+    //                 <img src={logo} className="App-logo" alt="logo" />
+    //                 <h1 className="App-title">Welcome to React</h1>
+    //             </header>
+    //             <p className="App-intro">
+    //                 To get started, edit <code>src/App.js</code> and save to reload.
+    //             </p>
+    //         </div>
+    //     );
+    // }
+}
+
+export default App;
